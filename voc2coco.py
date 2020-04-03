8bbef6112397cbc84892a8eb68701e5039deb674
Phạm Trọng Thắng <phamtrongthang123@gmail.com>
2020-04-03 12:51:26 +0700

    Update voc2coco.py

--- voc2coco.py
+++ voc2coco.py
@@ -59,6 +59,8 @@ def get_image_info(annotation_root, extract_num_from_imgid=True):
 
 def get_coco_annotation_from_obj(obj, label2id):
     label = obj.findtext('name')
+    if label == "truc":
+      label = "truck"
     assert label in label2id, f"Error: {label} is not in label2id !"
     category_id = label2id[label]
     bndbox = obj.find('bndbox')
